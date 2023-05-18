e661a67dfed967bc8af6b104dce47af04b9f249a
Luciano Cygler <lucianocygler@outlook.com>
2023-05-18 17:28:43 -0300

    first commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const router = require("./routes/index.js");
 const server = express();
 
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
 server.use(express.json());
 server.use(morgan("dev"));
-const corsOptions = {
-  origin: "https://vegan-world-deploy.vercel.app/",
-};
-
-server.use(cors(corsOptions));
+server.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://vegan-world-deploy.vercel.app/"
+  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 server.use("/", router);
 
