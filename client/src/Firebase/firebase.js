972034530395afd6919a4e589d6a07fa6725519f
Luciano Cygler <lucianocygler@outlook.com>
2023-05-18 15:23:09 -0300

    correcion

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
-dontenv.config();
+dotenv.config();
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIRE_BASE_KEY,
