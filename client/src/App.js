cb9d8ec26e0deda523436a7ae8fba32394f8bb84
Luciano Cygler <lucianocygler@outlook.com>
2023-05-18 15:45:05 -0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -27,13 +27,14 @@ function App() {
   //************************************* OCULTAR / MOSTRAR NAVBAR *********************************/
   const location = useLocation();
 
-  const showNav = location.pathname !== "/login";
-  const showNav2 = location.pathname !== "/register";
+  // const showNav = location.pathname !== "/login";
+  // const showNav2 = location.pathname !== "/register";
 
   return (
     <div className="App">
       <ChakraProvider />
-      {showNav && <NavBar />}
+      {/* {showNav && <NavBar />} */}
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* <Route path="/Login" element={<Login />} />
