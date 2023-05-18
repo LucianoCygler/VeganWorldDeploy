48f0e2b0bfabd5953408f65498fe89fd101f9c97
Luciano Cygler <lucianocygler@outlook.com>
2023-05-18 16:21:22 -0300

    first commit

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
+
+const {
+  REACT_APP_FIRE_BASE_KEY,
+  REACT_APP_FIRE_BASE_DOMAIN,
+  REACT_APP_FIRE_BASE_PROJECT_ID,
+  REACT_APP_FIRE_BASE_STORAGE_BUCKET,
+  REACT_APP_FIRE_BASE_SENDER_ID,
+  REACT_APP_FIRE_BASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
   apiKey: REACT_APP_FIRE_BASE_KEY,
   authDomain: REACT_APP_FIRE_BASE_DOMAIN,
