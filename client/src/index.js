08280442e1878d0a35e80c9d2cad6b926392476a
Luciano Cygler <lucianocygler@outlook.com>
2023-05-19 15:30:47 -0300

    test de origin

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import axios from "axios";
 import { ChakraProvider } from "@chakra-ui/react";
 
-axios.defaults.baseURL = "https://veganworlddeploy-production.up.railway.app";
+axios.defaults.baseURL = "https://veganworlddeploy-production.up.railway.app/";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
