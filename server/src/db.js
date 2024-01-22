8b33aef874004d6dfaf14b1461e16cb311c600d9
Luciano <lucianocygler@outlook.com>
2024-01-22 14:35:44 -0300

    ok

--- server/src/db.js
+++ server/src/db.js
@@ -20,6 +20,9 @@ const sequelize = new Sequelize(
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     dialectModule: pg,
+    dialectOptions: {
+      ssl: true,
+    },
   }
 );
 
