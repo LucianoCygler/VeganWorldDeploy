c9fc2c0481f47e6ff6abe34ab98bb6dc4e76f598
Luciano <lucianocygler@outlook.com>
2024-01-22 14:29:29 -0300

    ok

--- server/src/db.js
+++ server/src/db.js
@@ -17,8 +17,9 @@ const sequelize = new Sequelize(
   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   POSTGRES_URL,
   {
-    logging: false,
-    native: false,
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectModule: pg,
   }
 );
 
